4a7566b7e63234aed92140a243b83f4a2c12bd3e
Sohom Ghorai <80033329+SohomRik@users.noreply.github.com>
2021-06-30 23:33:43 +0530

    Update Sum of diagonal element.c

--- Sum of diagonal element.c
+++ Sum of diagonal element.c
@@ -1,39 +1,52 @@
 #include<stdio.h>
- 
-void main()
+
+int main()
 {
-    int mat[12][12];
-    int i,j,row,col,sum=0;
-    printf("Enter the number of rows and columns of the matrix\n");
-    scanf("%d%d",&row,&col);
-    printf("Enter the elements of the matrix\n");
-    for(i=0;i<row;i++)
+	int i,j,arr[3][3],sum=0;
+	
+	int *p = arr; 
+	
+	
+	printf("Enter the elements into the 3x3 matrix:\n");
+	for(i=0; i<3; i++)
+	{
+		for(j=0; j<3; j++)
+		{
+			scanf("%d", p);
+			p++; 
+		}
+	}
+	
+	
+    p = arr;
+	
+	
+    printf("\nOriginal Array:\n");
+    for (i=0; i<3; i++)
     {
-        for(j=0;j<col;j++)
+        for(j=0; j<3; j++)
         {
-            scanf("%d",&mat[i][j]);
-        }
+        
+        	printf("%d ", *p);
+        	p++; 	
+		}
+		printf("\n");
     }
- 
-    printf("The matrix\n");
-    for(i=0;i<row;i++)
+    
+
+    p = arr;
+    
+   
+    for (i=0; i<3; i++)
     {
-        for(j=0;j<col;j++)
+        for(j=0; j<3; j++)
         {
-            printf("%d\t",mat[i][j]);
-        }
-        printf("\n");
+        	if(i == j)
+        		sum += *p;
+        	p++;
+		}
     }
-    for(i=0;i<row;i++)
-    {
-        for(j=0;j<col;j++)
-        {
-            if(i==j)
-            {
-                sum=sum+mat[i][j];
-            }
-        }
-    }
- 
-    printf("The sum of diagonal elements of a square matrix = %d\n",sum);
+    
+    
+    printf("Sum of all diagonal elements of the 3x3 array: %d", sum);
 }
